674210ec0261200f4dea93d9a3465e344854eb11
justengel <jtengel08@gmail.com>
2020-10-02 22:59:18 -0400

    Updated to version 1.4.0. Renaming the install functions may break code for older versions.

--- pylibimport/__meta__.py
+++ pylibimport/__meta__.py
@@ -1,5 +1,5 @@
 name = 'pylibimport'
-version = '1.3.2'
+version = '1.4.0'
 description = 'Python utility for importing packages with the same name, but different version.'
 url = 'https://github.com/justengel/pylibimport'
 author = 'Justin Engel'
