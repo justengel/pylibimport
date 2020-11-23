cfa11dea4f03356db798884738f48fbfa222b1f5
justengel <jtengel08@gmail.com>
2020-11-23 10:06:46 -0500

    Updated pylibimport PyInstaller hook. Still cannot get pylibimport pip_proc to work with executable. This is mostly a problem when you are trying to install a directory with setup.py.

--- pyinstaller_hooks/hook-pylibimport.py
+++ pyinstaller_hooks/hook-pylibimport.py
@@ -1,4 +1,3 @@
-
 # from PyInstaller import config
 # from PyInstaller.utils.hooks import collect_data_files
 #
@@ -8,8 +7,29 @@
 #     'setuptools',
 #     'distutils',
 #     'pkg_resources',
-#     'pip._internal.commands.install'
 #     ]
+#
+# try:
+#     import pip._internal.commands
+#     hiddenimports.extend([v.module_path for v in pip._internal.commands.commands_dict.values()])
+# except (ImportError, AttributeError, Exception):
+#     hiddenimports.extend([
+#         'pip._internal.commands.install',
+#         'pip._internal.commands.download',
+#         'pip._internal.commands.uninstall',
+#         'pip._internal.commands.freeze',
+#         'pip._internal.commands.list',
+#         'pip._internal.commands.show',
+#         'pip._internal.commands.check',
+#         'pip._internal.commands.config',
+#         'pip._internal.commands.search',
+#         'pip._internal.commands.cache',
+#         'pip._internal.commands.wheel',
+#         'pip._internal.commands.hash',
+#         'pip._internal.commands.completion',
+#         'pip._internal.commands.debug',
+#         'pip._internal.commands.help',
+#         ])
 
 
 try:
