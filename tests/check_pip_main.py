f20a9f394aa42722e194f01fa784ee2f457460bf
justengel <jtengel08@gmail.com>
2020-12-15 13:05:30 -0500

    Updated check_pip_main.py testing code to reflext the order of arguments change for the install function.

--- tests/check_pip_main.py
+++ tests/check_pip_main.py
@@ -7,7 +7,7 @@ importer = pylibimport.VersionImporter(install_dir='./sub/target_dir')
 # importer.pip = pylibimport.pip_bin
 # importer.pip = pylibimport.pip_proc  # pip_proc uses light_process.LightProcess which allows this outside of main
 
-mod = importer.install('dynamicmethod', '1.0.4', './sub/import_dir/dynamicmethod-1.0.4-py3-none-any.whl')
+mod = importer.install('./sub/import_dir/dynamicmethod-1.0.4-py3-none-any.whl', 'dynamicmethod', '1.0.4')
 
 
 if __name__ == '__main__':
