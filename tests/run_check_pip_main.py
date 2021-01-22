d7516990fb21002be3f2047a406b47869d2b6cef
justengel <jtengel08@gmail.com>
2021-01-22 09:37:51 -0500

    change run check to only use pip_proc in main.

--- tests/run_check_pip_main.py
+++ tests/run_check_pip_main.py
@@ -1,16 +1,18 @@
 import pylibimport
-
-
-importer = pylibimport.VersionImporter(install_dir='./sub/target_dir')
-
-# importer.pip = pylibimport.pip_main
-# importer.pip = pylibimport.pip_bin
-# importer.pip = pylibimport.pip_proc  # pip_proc uses light_process.LightProcess which allows this outside of main
-
-mod = importer.install('./sub/import_dir/dynamicmethod-1.0.4-py3-none-any.whl', 'dynamicmethod', '1.0.4')
-
-pylibimport.run_pip('--help')
+import multiprocessing as mp
 
 
 if __name__ == '__main__':
+    importer = pylibimport.VersionImporter(install_dir='./sub/target_dir')
+
+    importer.pip = pylibimport.pip_main
+    # importer.pip = pylibimport.pip_bin
+
+    # mp.freeze_support()
+    # importer.pip = pylibimport.pip_proc_flag
+    # importer.pip = pylibimport.pip_proc  # pip_proc uses light_process.LightProcess which allows this outside of main
+
+    mod = importer.install('./sub/import_dir/dynamicmethod-1.0.4-py3-none-any.whl', 'dynamicmethod', '1.0.4')
+
+    pylibimport.run_pip('--help')
     print(mod)
