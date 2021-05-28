d25112ceb1c4f4215884a2d1303245a96816e7e1
justengel <jtengel08@gmail.com>
2021-05-28 15:41:33 -0400

    Updated to version 1.9.0

--- pylibimport/__meta__.py
+++ pylibimport/__meta__.py
@@ -1,5 +1,5 @@
 name = 'pylibimport'
-version = '1.8.3'
+version = '1.9.0'
 description = 'Python utility for importing packages with the same name, but different version.'
 url = 'https://github.com/justengel/pylibimport'
 author = 'Justin Engel'
